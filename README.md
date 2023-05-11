b923528c770263b9c25b2ef1124ffd4b53bea1bf
Andrew <andrew.dale.wylie@gmail.com>
2023-05-11 14:02:02 -0700

    Add first iteration of script

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# yahoo-finance-ledger-cli
+## yahoo-finance-ledger-cli
+
 Grab historical stock prices from Yahoo Finance and output in Ledger CLI format.
+
+
+### How to use
+
+Create a Python 3 virtual environment, activate it,
+and install the packages from `requirements.txt`.
+
+Modify the provided `.config` file to include any stock symbols
+you'd like to get price information for.
+Institution and account names will be included in the output
+to group the returned information (as shown below).
+
+Execute the script, eg. `python main.py 2023-05-10 -v`.
+The verbose flag is optional, though useful
+if there are many symbols to process (to view progress).
+The output is saved by default in `output.txt` file.
+
+Example `.config` file:
+
+```
+Institution:
+    AccountName:
+    - AAPL
+    - MSFT
+    - AMZN
+    - GOOGL
+    - FB
+```
+
+The generated `output.txt`:
+
+```
+; Institution
+; AccountName
+P 2023/05/10            AAPL           USD    173.56
+P 2023/05/10            MSFT           USD    312.31
+P 2023/05/10            AMZN           USD    110.19
+P 2023/05/10            GOOGL          USD    111.75
+P 2023/05/10            FB             USD    233.08
+```
---
+++ main.py
@@ -0,0 +1,108 @@
+import argparse
+import datetime
+import re
+import yaml
+
+import requests
+from bs4 import BeautifulSoup
+
+def load_configuration(file='.config'):
+    """
+    Load configuration from YAML file.
+
+    Format is like:
+
+    Institution:
+      AccountName:
+        - AAPL
+        - MSFT
+        - AMZN
+        - GOOGL
+        - FB
+
+    There can be any number of institutions and accounts specified.
+    
+    """
+    # Disclaimer: The stocks listed above are examples only,
+    # and are not to be taken as a recommendation or endorsement.
+    with open(file, 'r') as f:
+        return yaml.safe_load(f)
+
+
+def format_stock_price(date_str, symbol, currency, price):
+    # The date string is formatted like '2023-05-10', but we want to output like '2023/05/10'.
+    #
+    # For the output line,
+    #  the symbol starts at col 25, (so date width will be 25, less 2 (for 'P '), less 1 (ends at col 39))
+    #  currency at col 40, (so symbol width will be 15)
+    #  price decimal is placed at col 50. (so currency width will be 10 less space in front of decimal)
+    #
+    return f"P {date_str.replace('-', '/'):<22}{symbol:<15}{currency:<{10 - price.index('.')}}{price}"
+
+
+def get_stock_price(symbol, date_str):
+    """Return currency and price as a tuple."""
+    url = f'https://ca.finance.yahoo.com/quote/{symbol}/history'
+    firefox_user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0'
+    response = requests.get(url, headers={'User-Agent': firefox_user_agent})
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    date_text = datetime.datetime.strptime(date_str, '%Y-%m-%d').strftime('%B %d, %Y')
+
+    date_span = soup.find('span', string=date_text)
+
+    if date_span is None:
+        raise ValueError(f'No data available for {symbol} on {date_str}.')
+
+    # TODO, There doesn't seem to be a way to do a named capture group for the currency code.
+    currency_span = soup.find('span', string=re.compile(r'^Currency in \w{3}'))
+    currency = currency_span.contents[0].string[-3:]
+
+    price = (date_span
+        .parent
+        .next_sibling
+        .next_sibling
+        .next_sibling
+        .next_sibling
+        .contents[0]
+        .string
+    )
+
+    return currency, f'{float(price):.2f}'
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=(
+        'Get historical price information for given stock symbols '
+        'and return the data in a format suitable for Ledger CLI.'
+    ))
+    parser.add_argument('date', metavar='YYYY-MM-DD', type=str, help='A date in the format YYYY-MM-DD.')
+    parser.add_argument('--verbose', '-v', action='store_true', help='Enable verbose output.')
+    parser.add_argument(
+        '--output-file', '-o',
+        dest='output', metavar='output.txt',
+        type=str, default='output.txt',
+        help='Output file name, defaults to `output.txt`.'
+    )
+
+    args = parser.parse_args()
+
+    data = load_configuration()
+
+    output = []
+    for institution in data.keys():
+        if args.verbose:
+            print(f'Processing `{institution}` institution..')
+
+        output.append(f'; {institution}')
+
+        for account in data[institution].keys():
+            output.append(f'; {account}')
+            for symbol in data[institution][account]:
+                currency, price = get_stock_price(symbol, args.date)
+                output.append(format_stock_price(args.date, symbol, currency, price))
+
+    with open(args.output, 'w') as f:
+        f.write('\n'.join(output))
+        if args.verbose:
+            print(f'Output successfully written to {args.output}.')
